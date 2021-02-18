e629ea0d74b6928f01113f3387b8a38932a90513
prameksha <71688843+prameksha@users.noreply.github.com>
2021-02-18 20:15:14 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var canvas;
+var gameState = 0;
+var contestantCount;
+var database;
+var quiz;
+var question ;
+var contestant;  
+
+
+function setup(){
+  canvas = createCanvas(850,400);
+}
+
+
+function draw(){
+  background("pink");
+
+  
+}
